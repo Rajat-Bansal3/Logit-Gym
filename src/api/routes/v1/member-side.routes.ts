1844cf62fedace827c263aa7da4f0ed75fe819d9
rajat bansal <rajatbansal28082003@gmail.com>
2026-05-20 00:10:22 +0530

    fix: spelling

--- src/api/routes/v1/member-side.routes.ts
+++ src/api/routes/v1/member-side.routes.ts
@@ -9,16 +9,36 @@ router.use(authMiddleware);
 
 const memberController = new MemberController();
 
-router.get("/attendace", roleMiddleware("MEMBER"), memberController.getMemberAttendance);
+router.get(
+  "/attendance",
+  roleMiddleware("MEMBER"),
+  memberController.getMemberAttendance,
+);
 
-router.post("/attendance/:hash", roleMiddleware("MEMBER"), memberController.markAttendance);
+router.post(
+  "/attendance/:hash",
+  roleMiddleware("MEMBER"),
+  memberController.markAttendance,
+);
 
 router.get("/gym", roleMiddleware("MEMBER"), memberController.getMemberGym);
 
-router.get("/payments", roleMiddleware("MEMBER"), memberController.getMemberPayments);
+router.get(
+  "/payments",
+  roleMiddleware("MEMBER"),
+  memberController.getMemberPayments,
+);
 
 router.get("/profile", roleMiddleware("MEMBER"), memberController.profile);
-router.get("/dashboard", roleMiddleware("MEMBER"), memberController.getMemberDashboard);
-router.get("/occupancy", roleMiddleware("MEMBER"), memberController.getGymOccupancy);
+router.get(
+  "/dashboard",
+  roleMiddleware("MEMBER"),
+  memberController.getMemberDashboard,
+);
+router.get(
+  "/occupancy",
+  roleMiddleware("MEMBER"),
+  memberController.getGymOccupancy,
+);
 
 export default router;
